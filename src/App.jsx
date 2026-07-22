d0bfe5926dcb3f353a049b304649462ed5a9f929
angelsunday <angelakiskiriaki@gmail.com>
2026-07-22 11:04:31 +0300

    Component ClassCard and implemetation in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import ClassCard from './ClassCard'
 
 function App() {
   return (
     <div>
       <h1>Asana studio</h1>
       <p>Κλείσε το μάθημά σου εύκολα</p>
+      <ClassCard />
+      <ClassCard />
+      <ClassCard />
     </div>
   )
 }
---
+++ src/ClassCard.jsx
@@ -0,0 +1,10 @@
+function ClassCard() {
+    return (
+        <div>
+            <h3>Vinyasa flow</h3>
+            <p>18:00 · 60 λεπτά</p>
+        </div>
+    )
+}
+
+export default ClassCard
